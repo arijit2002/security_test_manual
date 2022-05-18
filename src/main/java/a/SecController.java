47c676c8082c9dfd730466b2d510b8bae0aa6d8b
Arijit Das <arijit10das2002@gmail.com>
2022-05-18 12:54:29 +0530

    adjustments

--- src/main/java/a/SecController.java
+++ src/main/java/a/SecController.java
@@ -4,8 +4,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @Controller
 public class SecController {
