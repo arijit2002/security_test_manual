2add5ec4acc7d2c35bdb4ae99644e84c7bc3c323
Arijit Das <arijit10das2002@gmail.com>
2022-05-18 11:06:36 +0530

    thymeleaf

--- src/main/java/a/SecController.java
+++ src/main/java/a/SecController.java
@@ -2,13 +2,14 @@ package a;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class SecController {
+	
 	@GetMapping("/")
 	public String index(@AuthenticationPrincipal User user) {
 		return "<h1>Landing Page.....Permitted to all!!!!!!!</h1>"
