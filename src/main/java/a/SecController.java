bc887c4cb8cd15024ea5922ce8aa74b3fb4c64ec
Arijit Das <arijit10das2002@gmail.com>
2022-05-19 14:35:09 +0530

    up

--- src/main/java/a/SecController.java
+++ src/main/java/a/SecController.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
@@ -24,4 +25,8 @@ public class SecController {
 		return "admin";
 	}
 	
+	@RequestMapping("/accessdenied")
+	public String accessdeniedpage() {
+		return "accessdenied";
+	}
 }
--- src/main/java/a/SecurityConfiguration.java
+++ src/main/java/a/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http)throws Exception{
-		http.csrf().disable()
+		http
 			.authorizeRequests()
 			.antMatchers("/").permitAll()
 			.antMatchers("/user").hasAnyRole("user","admin")
