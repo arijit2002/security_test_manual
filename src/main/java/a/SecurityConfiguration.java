082fb76161d6867b5941431efe82bee1d2c86aba
Arijit Das <arijit10das2002@gmail.com>
2022-05-19 11:05:45 +0530

    Update SecurityConfiguration.java

--- src/main/java/a/SecurityConfiguration.java
+++ src/main/java/a/SecurityConfiguration.java
@@ -29,7 +29,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http)throws Exception{
-		http.authorizeRequests()
+		http.csrf().disable()
+			.authorizeRequests()
 			.antMatchers("/").permitAll()
 			.antMatchers("/user").hasAnyRole("user","admin")
 			.antMatchers("/admin").hasRole("admin")
