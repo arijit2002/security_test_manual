b53250ad536f6feeebc63cb922ec93ca98f75383
Arijit Das <arijit10das2002@gmail.com>
2022-05-23 18:42:11 +0530

    error handled with custom page

--- src/main/java/a/MyErrorController.java
+++ src/main/java/a/MyErrorController.java
@@ -6,22 +6,25 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyErrorController implements ErrorController{
 	
-	@GetMapping(value="/error")
+	@RequestMapping("/error")
 	public String errorHandling(HttpServletRequest request) {
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 		if (status != null) {
 	        Integer statusCode = Integer.valueOf(status.toString());
 	    
 	        if(statusCode == HttpStatus.NOT_FOUND.value()) {
-	            return "404";
+	            return "error-404";
 	        }
 	        else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-	            return "500";
+	            return "error-500";
+	        }
+	        else if(statusCode == HttpStatus.FORBIDDEN.value()) {
+	        	return "error-403";
 	        }
 	    }
 		return "error";
